5f02b986c50c9acb27cdc8d81792e0bc02ee90fb
Gautham Vasan <gauthamv.529@gmail.com>
2025-02-04 11:12:52 -0700

    Add ClipAction wrapper and TD Error Scaler

--- avg.py
+++ avg.py
@@ -7,9 +7,10 @@ import gymnasium as gym
 import torch.nn.functional as F
 
 from torch.distributions import MultivariateNormal
-from gymnasium.wrappers import NormalizeObservation
+from gymnasium.wrappers import NormalizeObservation, ClipAction
 from datetime import datetime
 from incremental_rl.experiment_tracker import record_video
+from incremental_rl.td_error_scaler import TDErrorScaler
 
 
 def orthogonal_weight_init(m):
@@ -116,6 +117,8 @@ class AVG:
         self.qopt = torch.optim.Adam(self.Q.parameters(), lr=cfg.critic_lr, betas=cfg.betas)
 
         self.alpha, self.gamma, self.device = cfg.alpha_lr, cfg.gamma, cfg.device
+        self.td_error_scaler = TDErrorScaler()
+        self.G = 0
 
     def compute_action(self, obs):
         obs = torch.Tensor(obs.astype(np.float32)).unsqueeze(0).to(self.device)
@@ -127,6 +130,16 @@ class AVG:
         next_obs = torch.Tensor(next_obs.astype(np.float32)).unsqueeze(0).to(self.device)
         action, lprob = action.to(self.device), kwargs['lprob']
 
+        #### Return scaling
+        r_ent = reward - self.alpha * lprob.detach().item()
+        self.G += r_ent        
+        if done:
+            self.td_error_scaler.update(reward=r_ent, gamma=0, G=self.G)
+            self.G = 0
+        else:
+            self.td_error_scaler.update(reward=r_ent, gamma=self.cfg.gamma, G=None)
+        ####
+
         #### Q loss
         q = self.Q(obs, action.detach())    # N.B: Gradient should NOT pass through action here
         with torch.no_grad():
@@ -136,6 +149,7 @@ class AVG:
             target_V = q2 - self.alpha * next_lprob
 
         delta = reward + (1 - done) *  self.gamma * target_V - q
+        delta /= self.td_error_scaler.sigma
         qloss = delta ** 2
         ####
 
@@ -170,6 +184,7 @@ def main(args):
     env = gym.make(args.env)
     env = gym.wrappers.FlattenObservation(env)
     env = NormalizeObservation(env)
+    env = ClipAction(env)
 
     #### Reproducibility
     env.reset(seed=args.seed)
